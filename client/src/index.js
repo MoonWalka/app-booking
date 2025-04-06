fc2f94d962f016a054cd787c9fd2ba208c5845d6
MoonWalka <131055109+MoonWalka@users.noreply.github.com>
2025-04-06 23:58:03 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+    
+      
+        
+          
+          
+        
+      
+    
+  
+);
