0e23b3daf744421523aad885645f79c518877e2e
MoonWalka <131055109+MoonWalka@users.noreply.github.com>
2025-04-08 00:19:48 +0200

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,7 @@
 // client/src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +18,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig) ;
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
