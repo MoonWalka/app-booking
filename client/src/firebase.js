27c19ef2f5fae9fdeaa5b81ddd5fe0fa3aa54715
MoonWalka <131055109+MoonWalka@users.noreply.github.com>
2025-04-07 23:48:55 +0200

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,21 +1,21 @@
 // client/src/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Votre configuration Firebase (à récupérer depuis la console Firebase)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "VOTRE_AUTH_DOMAIN",
-  projectId: "VOTRE_PROJECT_ID",
-  storageBucket: "VOTRE_STORAGE_BUCKET",
-  messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
-  appId: "VOTRE_APP_ID"
+  apiKey: "AIzaSyCt994en0glR_WVbxxkDATXM25QV7HKovA",
+  authDomain: "app-booking-26571.firebaseapp.com",
+  projectId: "app-booking-26571",
+  storageBucket: "app-booking-26571.firebasestorage.app",
+  messagingSenderId: "985724562753",
+  appId: "1:985724562753:web:83a093ebd7a7034a9a85c0",
+  measurementId: "G-LC94BW3MWX"
 };
 
-// Initialiser Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-export { db, auth };
+const analytics = getAnalytics(app);
