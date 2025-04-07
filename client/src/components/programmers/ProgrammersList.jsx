115721b95b14a0b5b557caf2e0b1a414a4ed74b5
MoonWalka <131055109+MoonWalka@users.noreply.github.com>
2025-04-07 02:23:40 +0200

    Create ProgrammersList.jsx

---
+++ client/src/components/programmers/ProgrammersList.jsx
@@ -0,0 +1,13 @@
+// client/src/components/programmers/ProgrammersList.jsx
+import React from 'react';
+
+const ProgrammersList = () => {
+  return (
+    <div>
+      <h2>Liste des Programmateurs</h2>
+      <p>Cette fonctionnalité sera bientôt disponible.</p>
+    </div>
+  );
+};
+
+export default ProgrammersList;
